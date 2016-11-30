e4b61be7feec3ff6953f0f39f5f4a21c1d88651c
Alex Bazlinton <abazlinton@gmail.com>
2016-11-30 11:59:23 +0000

    basic food tests pass

---
+++ food.js
@@ -0,0 +1,8 @@
+var Food = function(name, health) {
+  this.name = name;
+  this.health = health;
+  this.poisonous = false;
+}
+
+
+module.exports = Food;
--- specs/food_spec.js
+++ specs/food_spec.js
@@ -1,8 +1,28 @@
 var assert = require("assert");
-var food = require("../food");
+var Food = require("../food");
 
 describe("Food", function(){
 
+  var bread;
+
+  beforeEach(function(){
+    bread = new Food("bread", 25);
+    console.log(bread);
+  });
+
+  it("has a name", function(){
+    assert.equal("bread", bread.name);
+
+  });
+
+  it("has health", function(){
+    assert.equal(25, bread.health);
+
+  })
+
+  it("starts with poisonous status of false", function(){
+    assert.equal(false, bread.poisonous);
+  })
 
 
 });
